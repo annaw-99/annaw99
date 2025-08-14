136617939b519055c7b31a85c2f09153852519b9
Tung-Yen Wang <annawang9909@gmail.com>
2025-08-14 16:43:07 -0400

    feat: main page card content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,10 +14,10 @@ export default function Home() {
         
         {/* Header */}
         <div className="flex flex-wrap justify-center gap-[25px]">
-          <div className="custom-shadow w-[45px] h-[45px] bg-[#E8E8E8] rounded-[30px] text-[20px]">A</div>
-          <div className="custom-shadow custom-button w-[250px] lg:w-[300px] 2xl:w-[410px] h-[45px] bg-[#E8E8E8] rounded-[30px] text-[16px]">ANNA_WANG@PORTFOLIO</div>
+          <div className="custom-shadow-center w-[45px] h-[45px] bg-[#E8E8E8] rounded-[30px] text-[20px]">A</div>
+          <div className="custom-shadow-center w-[250px] lg:w-[300px] 2xl:w-[410px] h-[45px] bg-[#E8E8E8] rounded-[30px] text-[16px]">ANNA_WANG@PORTFOLIO</div>
           <div 
-            className={`custom-shadow custom-button w-[140px] lg:w-[200px] 2xl:w-[245px] h-[45px] bg-[#E8E8E8] rounded-[30px] text-[16px] cursor-pointer ${
+            className={`custom-shadow-center custom-button w-[140px] lg:w-[200px] 2xl:w-[245px] h-[45px] bg-[#E8E8E8] rounded-[30px] text-[16px] text-[#616161] cursor-pointer ${
               showProjects ? 'pressed-shadow' : ''
             }`}
             onClick={() => setShowProjects(!showProjects)}>
@@ -26,14 +26,14 @@ export default function Home() {
 
           {/* Theme Colors */}
           <div 
-            className={`custom-shadow custom-button w-[65px] lg:w-[90px] h-[45px] bg-[#E8E8E8] rounded-[30px] text-[14px] text-[#616161] cursor-pointer ${
+            className={`custom-shadow-center custom-button w-[65px] lg:w-[90px] h-[45px] bg-[#E8E8E8] rounded-[30px] text-[14px] text-[#616161] cursor-pointer ${
               activeTheme === 'light' ? 'pressed-shadow' : ''
             }`}
             onClick={() => setActiveTheme('light')}>
             LIGHT
           </div>
           <div 
-            className={`custom-shadow custom-button w-[65px] lg:w-[90px] h-[45px] bg-[#E8E8E8] rounded-[30px] text-[14px] text-[#616161] cursor-pointer ${
+            className={`custom-shadow-center custom-button w-[65px] lg:w-[90px] h-[45px] bg-[#E8E8E8] rounded-[30px] text-[14px] text-[#616161] cursor-pointer ${
               activeTheme === 'dark' ? 'pressed-shadow' : ''
             }`}
             onClick={() => setActiveTheme('dark')}>
@@ -44,7 +44,7 @@ export default function Home() {
         {/* Main Content */}
         <div className="flex flex-col lg:flex-row gap-[25px] w-full max-w-[1200px]">
           
-          {/* Left Side - Profile/Project One + Links */}
+          {/* Left Portion */}
           <div className="flex flex-col gap-[25px] mx-auto">
             <div className="w-[320px] lg:w-[370px] 2xl:w-[481px] h-[400px] lg:h-[380px] 2xl:h-[479px] mx-auto"
                 style={{ perspective: '1000px' }}>
@@ -54,18 +54,31 @@ export default function Home() {
                   transformStyle: 'preserve-3d',
                   transform: showProjects ? 'rotateY(180deg)' : 'rotateY(0deg)'
                 }}>
-                {/* Front Side - Profile */}
+                {/* Front - Profile */}
                 <div 
-                  className="absolute inset-0 custom-shadow bg-[#E8E8E8] rounded-[30px] text-[20px] flex items-center justify-center"
+                  className="absolute inset-0 custom-shadow-left bg-[#E8E8E8] rounded-[30px] text-[20px] flex flex-col"
                   style={{ 
                     backfaceVisibility: 'hidden',
                     WebkitBackfaceVisibility: 'hidden'
                   }}>
-                  Profile
+                  <p className='text-[#A6A6A6] text-xs mb-2'>Hi! My name is <span className='text-[#616161]'>Anna (Tung-Yen)</span> and I'm a</p>
+                  <p className='heading-one text-[80px] leading-none mb-6'>Software Developer.</p>
+                  <p className='text-[#A6A6A6] text-xs'>Based in <span className='text-[#616161]'>New York City</span>.</p>
+
+                  <p className='text-[#A6A6A6] text-xs'>Graduated from <span className='text-[#616161]'>Cornell</span> with a
+                  <span className='text-[#616161]'> Master's </span>in <span className='text-[#616161]'>Information Science</span>.</p>
+
+                  <p className='text-[#A6A6A6] text-xs'>Specialize in <span className='text-[#616161]'>Full-Stack Development</span> and 
+                  <span className='text-[#616161]'> Data Analysis</span>.</p>
+                  
+                  <p className='text-[#A6A6A6] text-xs mb-6'>Currently learning more about <span className='text-[#616161]'>NLP</span> and 
+                  <span className='text-[#616161]'> Machine Learning</span>.</p>
+
+                  <p className='text-[#616161] text-xs'>Let's Connect!</p>
                 </div>
-                {/* Back Side - Project One */}
+                {/* Back - Project One */}
                 <div 
-                  className="absolute inset-0 custom-shadow bg-[#E8E8E8] rounded-[30px] text-[20px] flex items-center justify-center"
+                  className="absolute inset-0 custom-shadow-left bg-[#E8E8E8] rounded-[30px] text-[20px] flex items-center justify-center"
                   style={{ 
                     backfaceVisibility: 'hidden',
                     WebkitBackfaceVisibility: 'hidden',
@@ -79,18 +92,18 @@ export default function Home() {
             {/* Links */}
             <div className="flex gap-[24px]">
               <Link href="https://github.com/annaw-99" target="_blank" >
-                <div className="custom-shadow custom-button w-[65px] lg:w-[70px] 2xl:w-[100px] h-[40px] bg-[#E8E8E8] rounded-[30px] text-[12px] text-[#616161]">Github</div>
+                <div className="custom-shadow-center custom-button w-[65px] lg:w-[70px] 2xl:w-[100px] h-[40px] bg-[#E8E8E8] rounded-[30px] text-[12px] text-[#616161]">Github</div>
               </Link>
               <Link href="https://www.linkedin.com/in/tungyen-wang" target="_blank">
-                <div className="custom-shadow custom-button w-[65px] lg:w-[70px] 2xl:w-[100px] h-[40px] bg-[#E8E8E8] rounded-[30px] text-[12px] text-[#616161]">LinkedIn</div>
+                <div className="custom-shadow-center custom-button w-[65px] lg:w-[70px] 2xl:w-[100px] h-[40px] bg-[#E8E8E8] rounded-[30px] text-[12px] text-[#616161]">LinkedIn</div>
               </Link>
               <Link href="mailto:annawang9909@gmail.com">
-                <div className="custom-shadow custom-button w-[140px] lg:w-[180px] 2xl:w-[235px] h-[40px] bg-[#E8E8E8] rounded-[30px] text-[12px] text-[#616161]">annawang9909@gmail.com</div>
+                <div className="custom-shadow-center custom-button w-[140px] lg:w-[180px] 2xl:w-[235px] h-[40px] bg-[#E8E8E8] rounded-[30px] text-[12px] text-[#616161]">annawang9909@gmail.com</div>
               </Link>            
             </div>
           </div>
 
-          {/* Right Side - Skills/Project Two + Widgets/Project Three & Four */}
+          {/* Right Portion */}
           <div className="flex flex-col gap-[25px] mx-auto lg:mx-0">
             <div className="w-[320px] lg:w-[430px] 2xl:w-[481px] h-[200px] lg:h-[180px] 2xl:h-[205px] mx-auto"
                 style={{ perspective: '1000px' }}>
@@ -100,18 +113,35 @@ export default function Home() {
                   transformStyle: 'preserve-3d',
                   transform: showProjects ? 'rotateY(180deg)' : 'rotateY(0deg)'
                 }}>
-                {/* Front Side - Skills */}
+                {/* Front - Skills */}
                 <div 
-                  className="absolute inset-0 custom-shadow bg-[#E8E8E8] rounded-[30px] text-[16px] flex items-center justify-center"
+                  className="absolute inset-0 custom-shadow-left bg-[#E8E8E8] rounded-[30px] text-[16px] flex flex-col"
                   style={{ 
                     backfaceVisibility: 'hidden',
                     WebkitBackfaceVisibility: 'hidden'
                   }}>
-                  Skills
+                  <p className='text-[#616161] text-xs mb-2'>Tools I'm most familiar with</p>
+                  {/* Icons */}
+                  <div className='flex flex-row mb-2 gap-[10px]'>
+                    <div className='custom-shadow-center w-[45px] h-[45px] rounded-[30px]'>Java</div>
+                    <div className='custom-shadow-center w-[45px] h-[45px] rounded-[30px]'>Java</div>
+                    <div className='custom-shadow-center w-[45px] h-[45px] rounded-[30px]'>Java</div>
+                    <div className='custom-shadow-center w-[45px] h-[45px] rounded-[30px]'>Java</div>
+                    <div className='custom-shadow-center w-[45px] h-[45px] rounded-[30px]'>Java</div>
+                  </div>
+                  <div className='flex flex-row gap-[10px]'>
+                    <div className='custom-shadow-center w-[45px] h-[45px] rounded-[30px]'>Java</div>
+                    <div className='custom-shadow-center w-[45px] h-[45px] rounded-[30px]'>Java</div>
+                    <div className='custom-shadow-center w-[45px] h-[45px] rounded-[30px]'>Java</div>
+                    <div className='custom-shadow-center w-[45px] h-[45px] rounded-[30px]'>Java</div>
+                    <div className='custom-shadow-center w-[45px] h-[45px] rounded-[30px]'>Java</div>
+                  </div>
+
+
                 </div>
-                {/* Back Side - Project Two */}
+                {/* Back - Project Two */}
                 <div 
-                  className="absolute inset-0 custom-shadow bg-[#E8E8E8] rounded-[30px] text-[16px] flex items-center justify-center"
+                  className="absolute inset-0 custom-shadow-left bg-[#E8E8E8] rounded-[30px] text-[16px] flex items-center justify-center"
                   style={{ 
                     backfaceVisibility: 'hidden',
                     WebkitBackfaceVisibility: 'hidden',
@@ -130,18 +160,18 @@ export default function Home() {
                     transformStyle: 'preserve-3d',
                     transform: showProjects ? 'rotateY(180deg)' : 'rotateY(0deg)'
                   }}>
-                  {/* Front Side - Tictactoe */}
+                  {/* Front - Tictactoe */}
                   <div 
-                    className="absolute inset-0 custom-shadow bg-[#E8E8E8] rounded-[30px] text-[16px] flex items-center justify-center"
+                    className="absolute inset-0 custom-shadow-left bg-[#E8E8E8] rounded-[30px] text-[16px] flex flex-col"
                     style={{ 
                       backfaceVisibility: 'hidden',
                       WebkitBackfaceVisibility: 'hidden'
                     }}>
-                    Tictactoe
+                    <p className='text-[#616161] text-xs mb-2'>Tictactoe</p>
                   </div>
-                  {/* Back Side - Project Three */}
+                  {/* Back - Project Three */}
                   <div 
-                    className="absolute inset-0 custom-shadow bg-[#E8E8E8] rounded-[30px] text-[16px] flex items-center justify-center"
+                    className="absolute inset-0 custom-shadow-left bg-[#E8E8E8] rounded-[30px] text-[16px] flex items-center justify-center"
                     style={{ 
                       backfaceVisibility: 'hidden',
                       WebkitBackfaceVisibility: 'hidden',
@@ -159,18 +189,34 @@ export default function Home() {
                     transformStyle: 'preserve-3d',
                     transform: showProjects ? 'rotateY(180deg)' : 'rotateY(0deg)'
                   }}>
-                  {/* Front Side - Weather */}
+                  {/* Front - Weather */}
                   <div 
-                    className="absolute inset-0 custom-shadow bg-[#E8E8E8] rounded-[30px] text-[16px] flex items-center justify-center"
+                    className="absolute inset-0 custom-shadow-left bg-[#E8E8E8] rounded-[30px] text-[16px] flex flex-col"
                     style={{ 
                       backfaceVisibility: 'hidden',
                       WebkitBackfaceVisibility: 'hidden'
                     }}>
-                    Weather
+                    <p className='text-[#616161] text-xs mb-4'>Weather <span className='text-[#A6A6A6]'>in places I've been to</span></p>
+                    
+                    <div className='flex flex-col items-center justify-center'>
+                      {/* TODO: Integrate API */}
+                      <p className='text-[#616161] text-md mb-2'>Seattle</p>
+                      <div className='flex flex-row items-center justify-center gap-[20px] mb-4'>
+                        <div className='custom-shadow-center w-[30px] h-[30px] rounded-[30px]'></div>
+                        <div className='relative'>
+                          <p className='text-[#616161] text-[80px] leading-none'>23</p>
+                          <span className='absolute top-0 right-0 text-[#616161] text-xs'>°C</span>
+                        </div>                        
+                        <div className='custom-shadow-center w-[30px] h-[30px] rounded-[30px]'></div>
+                      </div>
+                      <div className='custom-shadow-center w-[150px] text-[#616161] px-4 py-2 rounded-[30px] text-xs'>Write something here.</div>
+
+                    </div>
+
                   </div>
-                  {/* Back Side - Project Four */}
+                  {/* Back - Project Four */}
                   <div 
-                    className="absolute inset-0 custom-shadow bg-[#E8E8E8] rounded-[30px] text-[16px] flex items-center justify-center"
+                    className="absolute inset-0 custom-shadow-left bg-[#E8E8E8] rounded-[30px] text-[16px] flex items-center justify-center"
                     style={{ 
                       backfaceVisibility: 'hidden',
                       WebkitBackfaceVisibility: 'hidden',
